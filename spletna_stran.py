862ec34b6626402e5781b22084fb9bca88392824
Teja Žvan <tz21697@student.uni-lj.si>
2026-07-25 16:41:50 +0200

    zapis funkcije, ki naloži spletno stran po potrebi in zapis dokumentacije funkcije

---
+++ spletna_stran.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+def shrani_spletno_stran_po_potrebi(url, ime_html_datoteke, mapa):
+    '''Funkcija, ki sprejme url, ime html datoteke in ime mape,
+    ta preveri če mapa že obstaja (če ne jo ustvari), potem pa
+    preveri če obstaja pot do datoteke (če obstaja ne naredi nič,
+    če ne pa pridobi html kodo iz url in shrani v html datoteko)
+    '''
+    pot = os.path.join(mapa, ime_html_datoteke)
+
+    os.makedirs(mapa, exist_ok=True)
+
+    if os.path.exists(pot):
+        pass
+    else:
+        html_koda = requests.get(url).text
+        with open(pot, 'w', encoding='utf-8') as f:
+            f.write(html_koda)

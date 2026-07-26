25b53bb387a4feec21238e3ca4c971f1372e1800
Teja Žvan <tz21697@student.uni-lj.si>
2026-07-26 20:26:18 +0200

    ustreznejše imenovanje datotek

--- seznam_v_csv.py
+++ seznam_v_csv.py
@@ -1,9 +1,12 @@
 import csv
+import os
 
 
-def shrani_v_csv(sez, ime_csv):
+def shrani_v_csv(sez, mapa, ime_csv):
     '''Funkcija zapiše podatke iz seznama v csv datoteko'''
-    with open(ime_csv, 'w', encoding='utf-8', newline='') as f:
+    pot = os.path.join(mapa, ime_csv)
+
+    with open(pot, 'w', encoding='utf-8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['ID', 'Priimek ime', 'Letnica rojstva', 'Država', 'Elo'])
 

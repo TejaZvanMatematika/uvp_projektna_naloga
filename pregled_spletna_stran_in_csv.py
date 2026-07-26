d3297152c89bc9b676320b2cad5211598daf211e
Teja Žvan <tz21697@student.uni-lj.si>
2026-07-26 15:51:10 +0200

    dodana funkcija za pregled obstoja csv in preimenovanje datoteke

--- pregled_spletna_stran_in_csv.py
+++ pregled_spletna_stran_in_csv.py
@@ -17,3 +17,18 @@ def shrani_spletno_stran_po_potrebi(url, ime_html_datoteke, mapa):
         html_koda = requests.get(url).text
         with open(pot, 'w', encoding='utf-8') as f:
             f.write(html_koda)
+
+
+def ustvari_csv_po_potrebi(mapa, ime_csv):
+    '''Funkcija, ki pogleda za obstoj mape (če mape ni jo ustvari),
+    in pogleda če je v mapi datoteka csv (če csv datoteke ni jo ustvari)
+    '''
+    pot = os.path.join(mapa, ime_csv)
+
+    os.makedirs(mapa, exist_ok=True)
+
+    if os.path.exists(pot):
+        pass
+    else:
+        with open(pot, 'w', encoding='utf-8') as f:
+            pass

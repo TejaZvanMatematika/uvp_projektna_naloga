3da8809bb818e65036dbb9a2358d7ac8631bc4e9
Teja Žvan <tz21697@student.uni-lj.si>
2026-07-27 15:49:33 +0200

    popravljanje funkcije v seznam.py

--- seznam.py
+++ seznam.py
@@ -8,19 +8,21 @@ def seznam_podatkov(html_koda):
     sez_vseh_podatkov = []
 
     vzorci = re.compile(
-        r'<a href="/profile/(?P<ID>\d+)">(?P<priimek_ime>[^<]+)</a>.*?'  # ID in priimek, ime
-        r'"\s*(?P<drzava>[A-Z]{3})\s*".*?'  # drzava
-        r'<td>(?P<elo>\d+)</td>.*?'  # elo
-        r'<td>(?P<leto_rojstva>\d+)</td>.*?',  # letnica rojstva
+        r'<td>\s*<a href=/profile/(?P<ID>\d+)>(?P<priimek_ime>[^<]+)</a>\s*</td>\s*'  # ID in priimek, ime
+        r'<td class="flag-wrapper">\s*'
+        r'<img[^>]+>\s*(?P<drzava>[A-Z]{3})\s*'  # država
+        r'</td>\s*'
+        r'<td>\s*(?P<elo>\d+)\s*</td>\s*'  # elo
+        r'<td>\s*(?P<leto_rojstva>\d+)\s*</td>',  # leto rojstva
         flags=re.DOTALL
-    )
+        )
 
     for vzorec in vzorci.finditer(html_koda):
         Id = int(vzorec.group('ID'))
         priimek_ime = vzorec.group('priimek_ime').strip()
         drzava = vzorec.group('drzava')
         elo = int(vzorec.group('elo'))
-        letnica_rojstva = int(vzorec.group('letnica_rojstva'))
+        letnica_rojstva = int(vzorec.group('leto_rojstva'))
 
         priimek_ime = priimek_ime.replace(',', '')
 

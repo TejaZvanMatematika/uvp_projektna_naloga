d935359e823fd63a14b135ac1ea20997ac9d3e8f
Teja Žvan <tz21697@student.uni-lj.si>
2026-07-27 22:17:48 +0200

    opazila vzorec in skrajšala main kodo, poskus delovanja s 2 spletnima stranema

--- main.py
+++ main.py
@@ -7,36 +7,26 @@ import seznam
 
 MAPA_HTML_KODE = 'HTML_kode'
 MAPA_CSV_DATOTEKE = 'CSV_datoteke'
-mape_in_kode.ustvari_csv_mapo('CSV_datoteke')
+
+mape_in_kode.ustvari_mapo(MAPA_HTML_KODE)
+mape_in_kode.ustvari_mapo(MAPA_CSV_DATOTEKE)
 
 
-# Top 100 Players July 2026
-URL_PLAYERS = 'https://ratings.fide.com/a_top.php?list=open'
-POT_HTML_KODA_PLAYERS = os.path.join(MAPA_HTML_KODE, '100_players_koda.html')
-
-mape_in_kode.shrani_spletno_stran_po_potrebi(URL_PLAYERS,
-                                             MAPA_HTML_KODE,
-                                             '100_players_koda.html')
-
-with open(POT_HTML_KODA_PLAYERS, 'r', encoding='utf-8') as f:
-    html_koda = f.read()
-
-SEZ_PLAYER = seznam.seznam_podatkov(html_koda)
-
-seznam_v_csv.shrani_v_csv(SEZ_PLAYER, MAPA_CSV_DATOTEKE, '100_players_podatki.csv')
+SEZ = [
+    ('https://ratings.fide.com/a_top.php?list=open', '100_players_koda.html',
+     '100_players_podatki.csv'),
+    ('https://ratings.fide.com/a_top.php?list=women', '100_wemen_koda.html',
+     '100_wemen_podatki.csv')
+]
 
 
-# Top 100 Wemen July 2026
-URL_WEMEN = 'https://ratings.fide.com/a_top.php?list=women'
-POT_HTML_KODA_WEMEN = os.path.join(MAPA_HTML_KODE, '100_wemen_koda.html')
+for sez in SEZ:
+    pot_html = os.path.join(MAPA_HTML_KODE, sez[1])
+    pot_csv = os.path.join(MAPA_CSV_DATOTEKE, sez[2])
 
-mape_in_kode.shrani_spletno_stran_po_potrebi(URL_WEMEN,
-                                             MAPA_HTML_KODE,
-                                             '100_wemen_koda.html')
+    mape_in_kode.shrani_spletno_stran_po_potrebi(sez[0], pot_html)
+    with open(pot_html, 'r', encoding='utf-8') as f:
+        html_koda = f.read()
 
-with open(POT_HTML_KODA_WEMEN, 'r', encoding='utf-8') as f:
-    html_koda = f.read()
-
-SEZ_WEMEN = seznam.seznam_podatkov(html_koda)
-
-seznam_v_csv.shrani_v_csv(SEZ_WEMEN, MAPA_CSV_DATOTEKE, '100_wemen_podatki.csv')
+    sez_podatki = seznam.seznam_podatkov(html_koda)
+    seznam_v_csv.shrani_v_csv(sez_podatki, pot_csv)
